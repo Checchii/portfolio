8f35caa95134e2315bb6ec29abb489041d470efb
Sebastian Checchi <sebastian.checchi@gmail.com>
2024-08-15 06:34:16 -0400

    app title

--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "./navigation.css";
 
+const scrollToTop = () => {
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    });
+}
+
 /// small little navigation component with my name as the home button - might add to this later -> drop down menu
 const Navigation = () => {
     return (
         <nav className="navbar">
             <div className="nav-container">
-                <Link to="/" className="nav-logo">
+                <Link to="/" className="nav-logo" onClick={scrollToTop}>
                     <img src={require('./sebastian-logo.png')} alt="" className="logo"></img>
                 </Link>
             </div>
